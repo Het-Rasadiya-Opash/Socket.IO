f2d10a9cb69aefc380024bcb2597c2a60df34f44
Het <hra.opash@gmail.com>
2026-03-18 10:29:46 +0530

    websocket

--- Basic-WS/server/index.js
+++ Basic-WS/server/index.js
@@ -1,2 +1,16 @@
 import express from "express";
+import { WebSocketServer } from "ws";
 
+const app = express();
+const server = app.listen(3000, () => {
+  console.log("server is running");
+});
+
+const wss =new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    console.log("Data from Client %s:", data);
+    ws.send("Thanks");
+  });
+});
